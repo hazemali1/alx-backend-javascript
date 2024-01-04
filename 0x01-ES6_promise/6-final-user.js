8c5816d0f7d7412da70c7208c60fc5386a4cd817
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-04 02:31:44 +0200

    settled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,9 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return (Promise.allSettled(promises)
+
+  return Promise.allSettled(promises)
     .then((results) => results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
-    }))));
+    })));
 }
