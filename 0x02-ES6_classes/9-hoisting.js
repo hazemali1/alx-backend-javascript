b7aa5b7af1a11d5e896248107a0978a5482fd1ec
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-05 01:54:35 +0200

    fix

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,22 +1,22 @@
-class HolbertonClass {
+export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
   }
 
   get year() {
-    return `this._year`;
+    return this._year;
   }
 
   get location() {
-    return `this._location`;
+    return this._location;
   }
 }
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-class StudentHolberton {
+export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
@@ -42,5 +42,4 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-const listOfStudents = [student1, student2, student3, student4, student5];
-export default listOfStudents;
+export const listOfStudents = [student1, student2, student3, student4, student5];
