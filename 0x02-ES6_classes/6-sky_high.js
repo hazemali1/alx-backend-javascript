6c5b0c51ff3d9a059925a5d5860f79e1d114b7a4
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-05 00:37:29 +0200

    inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,20 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  set sqft(sqft) { this._sqft = sqft; }
+
+  get sqft() { return this._sqft; }
+
+  set floors(floors) { this._floors = floors; }
+
+  get floors() { return this._floors; }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
