659de6be4442c277e96fb1cd81ef8659d41aff87
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-04 02:04:41 +0200

    settled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,22 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const arr = [];
-  return (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      results.forEach((result) => {
-        if (result.status === 'fulfilled') {
-          arr.push({
-            status: result.status,
-            value: result.value,
-          });
-        } else {
-          arr.push({
-            status: result.status,
-            value: result.reason,
-          });
-        }
-      });
-      return arr;
-    }));
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
+
+handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg')
+  .then((results) => {
+    console.log('Results:', results);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
