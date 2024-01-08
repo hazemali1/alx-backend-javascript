ced03e99f4d22c77dd83cb9bfd2a94901bdc3619
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-08 19:37:55 +0200

    task 8 :(

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, str) {
   const arr = [];
-  for (const i of set) {
+  for (const i of set.values()) {
     if (i.substr(0, str.length) && i.substr(0, str.length) === str) {
       arr.push(i.substr(str.length));
     }
